842881ca954a60ff3de2c4e1d493aef233bcc628
renanwilson <renan.alves.ra16@gmail.com>
2023-11-07 15:58:30 -0300

    fix: add host

--- server.js
+++ server.js
@@ -44,4 +44,4 @@ server.delete("/videos/:id", async (request, reply) => {
   return reply.status(204).send();
 });
 
-server.listen({ port: process.env.PORT ?? 3333 });
+server.listen({ host: "0.0.0.0", port: process.env.PORT ?? 3333 });
